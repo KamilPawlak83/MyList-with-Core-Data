33442b2309b715dd28af7c6ba71e02da1198d046
Kamil <kamilpawlak@op.pl>
2021-09-28 12:55:29 +0200

    Initial Commit

---
+++ MyList with Core Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyList with Core Data
+//
+//  Created by Kamil Pawlak on 28/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
