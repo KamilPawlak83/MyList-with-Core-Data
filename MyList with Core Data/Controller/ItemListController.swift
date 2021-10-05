0df7ffd57731789a80018a9fce86dc1014510fe0
Kamil <kamilpawlak@op.pl>
2021-10-05 12:51:06 +0200

    I change title in ItemListController - now we get title from categoryPressed

--- MyList with Core Data/Controller/ItemListController.swift
+++ MyList with Core Data/Controller/ItemListController.swift
@@ -16,6 +16,7 @@ class ItemListController: SwipeCellKitController {
     var categoryPressed : Category? {
         didSet{
             loadItems()
+            title = categoryPressed?.title
         }
     }
     
